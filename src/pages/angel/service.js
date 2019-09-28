c3a6fa271c0b532db366abb7e2f544fd897112e4
98May <98may@zju.edu.cn>
2019-09-28 18:12:54 +0800

    yeah

--- src/pages/angel/service.js
+++ src/pages/angel/service.js
@@ -2,7 +2,7 @@ import request from '@/utils/request';
 
 export async function fakeSubmitForm(params) {
   console.log(params)
-  return request('/api/top/angelInvest?'+'userId='+params.id_object+'money='+params.money, {
+  return request('/api/top/angelInvest?' + '&userId=' + params.id_object +'&money='+params.money, {
     method: 'POST',
     data: params,
   });
